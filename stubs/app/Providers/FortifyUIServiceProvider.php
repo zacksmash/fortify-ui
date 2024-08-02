6277c9c8bb3152df4e65347f97360c5d70fcb3b4
zacksmash <zachary.warren@me.com>
2024-08-02 09:01:31 -0600

    Fix missing use statement for request

--- stubs/app/Providers/FortifyUIServiceProvider.php
+++ stubs/app/Providers/FortifyUIServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Request;
 use Laravel\Fortify\Fortify;
 
 class FortifyUIServiceProvider extends ServiceProvider
